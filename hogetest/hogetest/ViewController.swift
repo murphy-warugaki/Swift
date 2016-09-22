800584c4b0dc8269283d938eed41f2adad56cff7
murphy-warugaki <murphydayo120@gmail.com>
2016-09-22 16:31:37 +0900

    a

---
+++ hogetest/hogetest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hogetest
+//
+//  Created by 西本真也 on 2016/09/22.
+//  Copyright © 2016年 murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
